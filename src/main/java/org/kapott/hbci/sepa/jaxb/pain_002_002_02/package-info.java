ccf61ff8175d1da5efb8da770fe4d0ddf140d04d
kamuffe <5285367+kamuffe@users.noreply.github.com>
2019-06-19 16:28:37 +0200

    fix build pipeline

--- src/main/java/org/kapott/hbci/sepa/jaxb/pain_002_002_02/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.11 generiert 
-// Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2019.06.19 um 02:07:01 PM CEST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:swift:xsd:$pain.002.002.02", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.kapott.hbci.sepa.jaxb.pain_002_002_02;
